87d19f8eecd00ce992f21304be74ebb1f906351e
kratikasuri <113634592+kratikasuri@users.noreply.github.com>
2024-05-18 16:43:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed("#text" , {
+    strings:["Frontend Developer", "Web Developer" , "UI/UX developer"],
+    typeSpeed: 50,
+    backSpeed:50,
+    backDelay: 500,
+    loop: true
+});
